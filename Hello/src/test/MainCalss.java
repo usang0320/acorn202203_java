2786c4eb35208b2ce28fba5742ef6fee1ca82b06
eusang <dbtkd98@naver.com>
2022-04-28 17:11:58 +0900

    hello commit

---
+++ Hello/src/test/MainCalss.java
@@ -0,0 +1,17 @@
+package test;
+
+public class MainCalss {
+	//run(실행) 했을 때 실행의 흐름이 시작 되는 main 메소드
+	public static void main(String[] args) {
+		
+		System.out.println("Hello, world!");
+		
+		String str="leeyusang";
+		
+		int num1=10;
+		
+		boolean isRun=true;
+		
+		
+	}
+}
