21d401ffdd8633b00bec20dd69cf0f5c3fbdeba4
eusang <dbtkd98@naver.com>
2022-04-29 17:23:49 +0900

    quiz 틀만 추가

---
+++ Step12_UtilClass/src/test/main/QuizMain.java
@@ -0,0 +1,13 @@
+package test.main;
+/*
+ *  run 을 누르면 로또번호 6 개를 랜덤하게 출력하는 코드를 작성해 보세요.
+ *  
+ *  1~45 사이의 숫자 중에 6개가 나와야 하며
+ *  중복되는 숫자가 있으면 안되고
+ *  작은 숫자부터 큰숫자 까지 정렬해서 출력해보세요.
+ */
+public class QuizMain {
+	public static void main(String[] args) {
+		
+	}
+}
