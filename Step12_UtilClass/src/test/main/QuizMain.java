0de0390b6e93e1572e290ead099424641002b379
eusang <dbtkd98@naver.com>
2022-04-29 17:39:13 +0900

    Quiz 풀이

--- Step12_UtilClass/src/test/main/QuizMain.java
+++ Step12_UtilClass/src/test/main/QuizMain.java
@@ -1,4 +1,10 @@
 package test.main;
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+import java.util.TreeSet;
+
 /*
  *  run 을 누르면 로또번호 6 개를 랜덤하게 출력하는 코드를 작성해 보세요.
  *  
@@ -8,6 +14,14 @@ package test.main;
  */
 public class QuizMain {
 	public static void main(String[] args) {
+		Random ran = new Random();
+		TreeSet<Integer> nums=new TreeSet<>();
+		
+		while(nums.size()<6) {
+			int ranNum=ran.nextInt(45)+1;
+			nums.add(ranNum);
+		}
+		System.out.println("이번주 당첨번호는 "+nums+"입니다.");
 		
 	}
 }
