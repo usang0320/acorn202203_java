2deb7136316bc982b63b4a61dc750af9a08b871b
eusang <dbtkd98@naver.com>
2022-05-02 10:08:20 +0900

    Step12 에 QuizMain.java 추가함

--- Step12_UtilClass/src/test/main/QuizMain.java
+++ Step12_UtilClass/src/test/main/QuizMain.java
@@ -1,8 +1,12 @@
 package test.main;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Random;
 import java.util.Set;
+import java.util.Stack;
 import java.util.TreeSet;
 
 /*
@@ -22,6 +26,32 @@ public class QuizMain {
 			nums.add(ranNum);
 		}
 		System.out.println("이번주 당첨번호는 "+nums+"입니다.");
+		
+		
+		//중복된 데이터가 저장되지 않는 정수를 저장할 수 있는 Stack 객체 생성
+		Set<Integer> st = new HashSet<>();
+		
+		//무한루프 돌면서
+		while(true) {
+			///랜덤한 숫자를 얻어내서
+			int ranNum = ran.nextInt(45)+1;
+			//Stack 객체에 누적시키기
+			st.add(ranNum);
+			//번호가 6개가 되면
+			if(st.size()==6) {
+				break;	//반복문 탈출
+			}
+		}
+		System.out.println(st);
+		//Set 에 있는 데이터를 ArrayList 에 담고
+		List<Integer> numbers = new ArrayList<>(st);
+		System.out.println(numbers);
+		//정렬하고
+		Collections.sort(numbers);
+		//출력
+		for(int tmp:numbers) {
+			System.out.println(tmp+",");
+		}
 		
 	}
 }
